7a33663792b30bd118834f6295fffbfa4a2548c6
Agata Momot <am406133@students.mimuw.edu.pl>
2023-11-27 13:28:00 +0100

    Should be OK if I remove debug printouts, otherwise grep returns 1 if the search failed

--- Makefile
+++
@@ -1,9 +0,0 @@
-TARGET = typechecker
-SRC_DIR = ./src
-GRAMMAR_DIR = Latte
-
-all: $(SRC_DIR)/$(TARGET).hs
-	ghc -i$(SRC_DIR) $(SRC_DIR)/$(TARGET).hs -o ./$(TARGET)
-
-clean:
-	$(RM) $(TARGET) $(SRC_DIR)/*.o $(SRC_DIR)/*.hi
--- test.sh
+++ test.sh
@@ -1,17 +1,15 @@
-ghc typechecker.hs
-
-for file in `ls ./lattests/good/*lat`
+for file in `ls ../lattests/good/*lat`
 do
 	echo "$file"
-	cat "$file" | ./src/typechecker | grep -e "OK|ERROR"
+	cat "$file" | ./typechecker | grep -e "OK|ERROR"
 	echo $?
 done
 
 echo -e "\nBAD"
 
-for file in `ls ./lattests/bad/*lat`
+for file in `ls ../lattests/bad/*lat`
 do
         echo "$file"
-        cat "$file" | ./src/typechecker | grep "OK|ERROR"
+        cat "$file" | ./typechecker | grep "OK|ERROR"
 	echo $?
 done
