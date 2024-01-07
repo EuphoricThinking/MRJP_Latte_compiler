90e0281cc779af44a85692bbb37cbf4d23f13737
Agata Momot <am406133@students.mimuw.edu.pl>
2024-01-07 08:54:50 +0100

    Changed runtime, added error()

--- program/src/runtime.c
+++ program/src/runtime.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // gcc runtime.c -c -o ../lib/runtime.o
 
 void printInt(int a) {
-    printf("%d", a);
+    printf("%d\n", a);
+}
+
+void error() {
+    fprintf(stderr, "runtime error\n");
+    exit(EXIT_FAILURE);
 }
