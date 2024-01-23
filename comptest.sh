b2833ad38dce8bbfe1077f530a9bae1a96ad5631
Agata Momot <am406133@students.mimuw.edu.pl>
2024-01-23 16:51:55 +0100

    Modified tests

--- comptest.sh
+++ comptest.sh
@@ -1,4 +1,5 @@
 make
 ./ltc ../lattests/good/core001.lat
-./ltc ../playground/extStage.lat
+#./ltc ../playground/extStage.lat
+./ltc ../lattests/extensions/objects1/queue.lat
 # cat ../lattests/good/core001.s
